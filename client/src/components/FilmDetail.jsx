ad3e15f075f6f770a8756c9fc1fe52c536457e1e
Janet Jiang <janetjiang1103@gmail.com>
2023-10-03 20:49:37 +0800

    change card img src to be relative to the url in database

--- client/src/components/FilmDetail.jsx
+++ client/src/components/FilmDetail.jsx
@@ -22,7 +22,7 @@ const FilmDetail = (props) => {
         <Card>
             <Card.Body>
             <Card.Title>{props.film.name}</Card.Title>
-            <Card.Img variant="top" src="https://amc-theatres-res.cloudinary.com/image/upload/f_auto,fl_lossy,h_465,q_auto,w_310/v1694448805/amc-cdn/production/2/movies/70800/70810/PosterDynamic/157067.jpg" style={{ width: '20%', height: '20%' }}  />
+            <Card.Img variant="top" src={props.film.url} style={{ width: '20%', height: '20%' }}  />
             <Card.Text>
                 <div>
                     Film Description: {props.film.description}
